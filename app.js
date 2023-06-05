5c176c237a4dec6dc36763d6190402e982d88afa
Tomas Roun <tomas.roun8@gmail.com>
2023-06-05 20:14:53 +0200

    Update website

--- app.js
+++ app.js
@@ -6,10 +6,12 @@ const server = livereload.createServer();
 
 server.watch(path.join(__dirname, "phrases"));
 server.watch(path.join(__dirname, "flashcards"));
+server.watch(path.join(__dirname, "public"));
 
 const app = express();
 app.use(connect());
-app.use('/phrases', express.static("phrases/public"));
-app.use('/flashcards', express.static("flashcards"));
+app.use("/", express.static("public"));
+app.use("/phrases", express.static("phrases/public"));
+app.use("/flashcards", express.static("flashcards"));
 
-app.listen(8080, () => console.log("Express is listening.."));
+app.listen(8081, () => console.log("Express is listening.."));
